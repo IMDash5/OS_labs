485454f0896e665a0ca14b55394dddc1ead5d254
Ivan Mavrin <ivanm@MacBook-Air-Ivan.local>
2024-10-20 18:53:32 +0300

    lab approved

--- lab1/child.c
+++ lab1/child.c
@@ -3,31 +3,31 @@
 #include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdbool.h>
 
 #define BUFFER_SIZE 1024
 
+bool is_vowel(char c) {
+    return strchr("AEIOUYaeiouy", c) != NULL;
+}
+
 void remove_vowels(char *str) {
-    char *p = str;
-    char *q = str;
-    while (*p) {
-        if (!strchr("AEIOUYaeiouy", *p)) {
-            *q++ = *p;
+    int write_index = 0;
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (!is_vowel(str[i])) {
+            str[write_index++] = str[i];
         }
-        p++;
     }
-    *q = '\0';
+    
+    str[write_index] = '\0';
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s <process_number> <output_file>\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
 
     int process_number = atoi(argv[1]);
     char *filename = argv[2];
 
-    // Открываем файл для записи
     int file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
     if (file == -1) {
         perror("Error opening file");
@@ -37,13 +37,11 @@ int main(int argc, char *argv[]) {
     char buffer[BUFFER_SIZE];
     ssize_t bytes_read;
 
-    // Чтение строк из stdin (перенаправлено из pipe)
     while ((bytes_read = read(STDIN_FILENO, buffer, sizeof(buffer) - 1)) > 0) {
         buffer[bytes_read] = '\0';
-        remove_vowels(buffer);  // Удаление гласных
-        write(file, buffer, strlen(buffer));  // Запись в файл
+        remove_vowels(buffer);
+        write(file, buffer, strlen(buffer));
 
-        // Вывод в стандартный вывод для child1 или child2
         if (process_number == 1) {
             printf("Child1 processed: %s", buffer);
         } else if (process_number == 2) {
--- lab1/parent.c
+++ lab1/parent.c
@@ -3,7 +3,6 @@
 #include <unistd.h>
 #include <string.h>
 #include <fcntl.h>
-#include <sys/wait.h>
 #include <time.h>
 
 #define BUFFER_SIZE 1024
@@ -18,66 +17,53 @@ int main() {
 
     srand(time(NULL));
 
-    // Создаем два pipe
     if (pipe(pipe1) == -1 || pipe(pipe2) == -1) {
         perror("Pipe failed");
         return 1;
     }
 
-    // Получаем имена файлов от пользователя
     char filename1[BUFFER_SIZE], filename2[BUFFER_SIZE];
     printf("Enter filename for child1: ");
     fgets(filename1, sizeof(filename1), stdin);
-    filename1[strcspn(filename1, "\n")] = '\0';  // Удаляем символ новой строки
+    filename1[strcspn(filename1, "\n")] = '\0';
 
     printf("Enter filename for child2: ");
     fgets(filename2, sizeof(filename2), stdin);
-    filename2[strcspn(filename2, "\n")] = '\0';  // Удаляем символ новой строки
-
-    // Создание первого дочернего процесса
+    filename2[strcspn(filename2, "\n")] = '\0';
     if ((child1 = fork()) == 0) {
-        // Перенаправляем каналы на дочерний процесс
-        close(pipe1[1]);  // Закрываем конец записи pipe1 в дочернем процессе
-        dup2(pipe1[0], STDIN_FILENO);  // Перенаправляем pipe1 в стандартный ввод
+        close(pipe1[1]); 
+        dup2(pipe1[0], STDIN_FILENO);
 
-        // Запускаем дочерний процесс 1
         execl("./child", "child", "1", filename1, NULL);
         perror("execl failed");
         exit(EXIT_FAILURE);
     }
 
-    // Создание второго дочернего процесса
     if ((child2 = fork()) == 0) {
-        // Перенаправляем каналы на дочерний процесс
-        close(pipe2[1]);  // Закрываем конец записи pipe2 в дочернем процессе
-        dup2(pipe2[0], STDIN_FILENO);  // Перенаправляем pipe2 в стандартный ввод
+        close(pipe2[1]);
+        dup2(pipe2[0], STDIN_FILENO);
 
-        // Запускаем дочерний процесс 2
         execl("./child", "child", "2", filename2, NULL);
         perror("execl failed");
         exit(EXIT_FAILURE);
     }
 
-    // Родительский процесс
-    close(pipe1[0]);  // Закрываем конец чтения pipe1
-    close(pipe2[0]);  // Закрываем конец чтения pipe2
+    close(pipe1[0]); 
+    close(pipe2[0]);  
 
     char buffer[BUFFER_SIZE];
 
-    // Чтение строк от пользователя и отправка в соответствующий pipe
     printf("Enter strings (type 'exit' to quit):\n");
     while (fgets(buffer, sizeof(buffer), stdin)) {
-        if (strcmp(buffer, "exit\n") == 0) break;  // Завершение программы
+        if (strcmp(buffer, "exit\n") == 0) break;
 
-        // Отправляем строку в pipe1 с вероятностью 80%, иначе в pipe2
         if (random_80_percent()) {
-            write(pipe1[1], buffer, strlen(buffer));  // В pipe1
+            write(pipe1[1], buffer, strlen(buffer));
         } else {
-            write(pipe2[1], buffer, strlen(buffer));  // В pipe2
+            write(pipe2[1], buffer, strlen(buffer)); 
         }
     }
 
-    // Закрываем концы записи в pipe
     close(pipe1[1]);
     close(pipe2[1]);
 
